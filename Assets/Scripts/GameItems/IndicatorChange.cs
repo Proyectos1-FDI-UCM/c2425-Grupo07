427b3c157d57f281656ccddf2ce2a91dc042a7f4
LailaXD54 <lilingch@ucm.es>
2025-04-25 11:07:03 +0200

    push indicador para intrucciones

---
+++ Assets/Scripts/GameItems/IndicatorChange.cs
@@ -0,0 +1,117 @@
+//---------------------------------------------------------
+// Se programa el paso de imagenas para las instrucciones del juego
+// Liling Chen
+// Clank & Clutch
+// Proyectos 1 - Curso 2024-25
+//---------------------------------------------------------
+
+using UnityEngine;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Antes de cada class, descripción de qué es y para qué sirve,
+/// usando todas las líneas que sean necesarias.
+/// </summary>
+public class IndicatorChange : MonoBehaviour
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // públicos y de inspector se nombren en formato PascalCase
+    // (palabras con primera letra mayúscula, incluida la primera letra)
+    // Ejemplo: MaxHealthPoints
+
+    [SerializeField] private Sprite[] Page;
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // privados se nombren en formato _camelCase (comienza con _, 
+    // primera palabra en minúsculas y el resto con la 
+    // primera letra en mayúsculas)
+    // Ejemplo: _maxHealthPoints
+
+    private int _num = 0;
+    private SpriteRenderer _page;
+    private bool _on = false;
+    #endregion
+
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before 
+    /// any of the Update methods are called the first time.
+    /// </summary>
+    void Start()
+    {
+        _page = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+    public void Pass() //flecha para avanzar
+    {
+        if (_num < Page.Length)
+        {
+            _num++;
+            ChangePage();
+        }
+    }
+
+    public void Return() //flecha para retroceder
+    {
+        if (_num > 0)
+        {
+            _num--;
+            ChangePage();
+        }
+    }
+
+    public void ChangePage() //Cambia la imagen del Game Object al del siguiente o anterior de la array
+    {
+        _page.sprite = Page[_num];
+    }
+
+    public void HideShowImage()
+    {
+        if (_on)
+        {
+            _on = false;
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            _on = true;
+            gameObject.SetActive(true);
+        }
+    }
+
+    #endregion
+
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+    // Documentar cada método que aparece aquí
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+
+    #endregion
+
+} // class IndicatorChange 
+// namespace
